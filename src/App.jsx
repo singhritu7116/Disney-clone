2fdae67f79b60b188a7645629cb5dc9fcedf6f0e
RITU KUMARI <102386249+singhritu7116@users.noreply.github.com>
2024-09-06 15:26:11 +0530

    abc

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import GenreMovieList from './Componets/GenreMovieList'
+import Header from './Componets/Header'
+import ProductionHouse from './Componets/ProductionHouse'
+import Slider from './Componets/Slider'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Slider/>
+      <ProductionHouse/>
+      
+      <GenreMovieList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componets/GenreMovieList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import MovieList from './MovieList'
+import GenresList from '../Constants/GenresList'
+
+function GenreMovieList() {
+  return (
+    <div>
+        {GenresList.genere.map((item,index)=>index<=4&&(
+            <div className='p-8 px-8 md:px-16'>
+                <h2 className='text-[20px] text-white 
+                font-bold'>{item.name}</h2> 
+                <MovieList genreId={item.id} index_={index} />   
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default GenreMovieList
